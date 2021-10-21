1f5d661b4916c962e9eb2dd4689465e8538fec6d
vfreiberg <>
2021-10-21 14:49:03 +0300

    add bash script to apply config changes

---
+++ set_config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for file_name in '.tmux.conf' '.Xresources' '.config/nvim/init.vim' ;
+  do  
+    src_path="$(pwd)/${file_name}"
+    target_path="${HOME}/${file_name}"
+    echo "Creating symlink to ${src_path} at ${target_path}..."
+    ln -sf $src_path $target_path
+  done
+echo 'Done!'
