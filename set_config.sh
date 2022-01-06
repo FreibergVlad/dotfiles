b9b18863fbae1b76f0bf4bbcc02ba7de3dafe268
vfreiberg <>
2022-01-06 17:03:37 +0300

    add .Xresources reloading to startup script

--- set_config.sh
+++ set_config.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 for file_name in '.tmux.conf' '.Xresources' '.config/nvim/init.vim' ;
-  do  
-    src_path="$(pwd)/${file_name}"
-    target_path="${HOME}/${file_name}"
-    echo "Creating symlink to ${src_path} at ${target_path}..."
-    ln -sf $src_path $target_path
-  done
+    do
+        src_path="$(pwd)/${file_name}"
+        target_path="${HOME}/${file_name}"
+        echo "Creating symlink to ${src_path} at ${target_path}..."
+        ln -sf $src_path $target_path
+    done
+
+echo 'Reloading .Xresources file...'
+xrdb ${HOME}/.Xresources
 echo 'Done!'
