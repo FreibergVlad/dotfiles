2c075a7bae0fb9a2ef8fc2f00f0b789ba7d29ed3
vfreiberg <>
2022-09-08 14:56:39 +0400

    move from rxvt-unicode to alacritty

--- set_config.sh
+++ set_config.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir -p "${HOME}/.config/nvim"
+configs=(
+    ".tmux.conf"
+    ".alacritty.yml"
+    ".config/nvim/init.vim"
+)
 
-for file_name in '.tmux.conf' '.Xresources' '.config/nvim/init.vim' ;
+mkdir -p ${HOME}/.config/nvim
+
+for config in "${configs[@]}";
     do
-        src_path="$(pwd)/${file_name}"
-        target_path="${HOME}/${file_name}"
-        echo "Creating symlink to ${src_path} at ${target_path}..."
+        src_path="$(pwd)/${config}"
+        target_path="${HOME}/${config}"
+        echo "Creating symlink to ${src_path} at ${target_path}"
         ln -sf $src_path $target_path
     done
-
-echo 'Reloading .Xresources file...'
-xrdb ${HOME}/.Xresources
-echo 'Done!'
