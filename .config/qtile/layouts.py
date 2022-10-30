8008146828da68ad3b927df04c90b59240c8affa
vfreiberg <>
2022-10-30 15:51:45 +0400

    add styles for floating windows

--- .config/qtile/layouts.py
+++ .config/qtile/layouts.py
@@ -10,12 +10,18 @@ layout_defaults = dict(
     border_focus=color_schema['yellow'],
     border_normal=color_schema['bg'],
 )
+floating_defaults = dict(
+    margin=5,
+    border_focus=color_schema['red'],
+    border_normal=color_schema['yellow'],
+)
 layouts = [
     layout.Max(**layout_defaults),
     layout.Columns(**layout_defaults),
-    layout.Floating(**layout_defaults),
+    layout.Floating(**floating_defaults),
 ]
 floating_layout = layout.Floating(
+    **floating_defaults,
     # Run the utility of `xprop` to see the wm class and name of an X client
     float_rules=[
         *layout.Floating.default_float_rules,
